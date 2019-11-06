a1e10d0d9901e6362b61151396423f3aea9088d6
Simon Grimm <saimon@devdactic.com>
2019-11-06 14:15:09 +0100

    Fix script.

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -12,6 +12,7 @@ export class ConfigService {
     ) {
       this.envConfig = {
         MONGODB_URI: process.env.MONGODB_URI,
+        JWT_SECRET: process.env.JWT_SECRET,
       };
     } else {
       this.envConfig = dotenv.parse(fs.readFileSync('.env'));
